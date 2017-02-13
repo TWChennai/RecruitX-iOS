72c3fa7ecba60b89e1e33e07ff5ad4a1f207b060
Arun Sundaralingam <arunsundaralingam@IN-CHNI-Arun-Sundaralingam.local>
2017-02-13 18:58:50 +0530

    [Arun/Priya] Add my interviews page

---
+++ RecruitX/InterviewCell.swift
@@ -0,0 +1,15 @@
+//
+//  InterviewCell.swift
+//  RecruitX
+//
+//  Created by Arun Sundaralingam on 13/02/17.
+//  Copyright © 2017 Thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class InterviewCell: UITableViewCell {
+    @IBOutlet weak var intervieweeName: UILabel!
+    @IBAction func buttonClicked(_ sender: UIButton) {
+    }
+}
