9dea1b17e829706c9b91dff1d0ae7ab56f1e1975
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2017-02-13 18:31:03 +0530

    [Arun/Priya] Add my interviews page

--- RecruitX/SecondViewController.swift
+++ RecruitX/SecondViewController.swift
@@ -1,23 +1,24 @@
-//
-//  SecondViewController.swift
-//  RecruitX
-//
-//  Created by Venkateswari Srinivasan on 13/02/17.
-//  Copyright © 2017 Thoughtworks. All rights reserved.
-//
-
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    @IBOutlet weak var MyTable: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell123", for: indexPath) as! InterviewCell
+        cell.intervieweeName!.text = "Interviewee Name"
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
